20f89922287cf30619cbba0d493e38f32a147668
Tarun Agarwal <tarun.agarwal2004@gmail.com>
2022-02-27 13:45:15 +0530

    Catalan Numbers

---
+++ Combinatorics/catalan.h
@@ -0,0 +1,25 @@
+// Catalan numbers
+#pragma once
+#ifndef BOUTIQUE_CATALAN
+#define BOUTIQUE_CATALAN
+
+//#include "pnc.h"
+#include <cmath>
+#include <numbers>
+
+namespace btq {
+    int catalanNumbersCache[15] = {1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796, 58786, 208012, 742900, 2674440};
+    long long cat(int n) {
+        if (n < 15)
+            return catalanNumbersCache[n];
+        return pow(4, n) * std::tgamma(n + 0.5) * std::numbers::inv_sqrtpi_v<long double>/std::tgamma(n+2);
+        
+        // can also implement in terms of ncr
+        // return btq::ncr(2*n, n)/(n+1);
+        
+        
+    }
+
+} // namespace boutique
+
+#endif // BOUTIQUE_CATALAN_H
