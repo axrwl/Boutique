ea89cddec29e3a92d3c1166d55a50b61c2d4e8cb
Tarun Agarwal <tarun.agarwal2004@gmail.com>
2022-03-03 23:00:06 +0530

    Permutation sort standard container friendly

--- Sort/permutation.h
+++ Sort/permutation.h
@@ -6,14 +6,19 @@
 #include <algorithm>
 
 namespace btq {
+    template<typename It>
+    void permutationSort(It begin, It end) {
+        while (std::next_permutation(begin, end));
+    }
+
     template<typename T>
     void permutationSort(T *array, int a, int b) {
-        while (std::next_permutation(array+a, array+b+1));
+        return btq::permutationSort(array+a, array+b+1);
     }
 
     template<typename T>
     void permutationSort(T *array, int n) {
-        return permutationSort(array, 0, n-1);
+        return btq::permutationSort(array, 0, n-1);
     }
 
 } // namespace boutique
