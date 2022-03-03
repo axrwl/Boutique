978b308410ee17a54683b15c41eaa659089bbc20
Tarun Agarwal <tarun.agarwal2004@gmail.com>
2022-03-03 23:05:15 +0530

    Selection sort standard container friendly

--- Sort/selection.h
+++ Sort/selection.h
@@ -3,14 +3,19 @@
 #ifndef BOUTIQUE_SELECTION_SORT
 #define BOUTIQUE_SELECTION_SORT
 
-
+#include <algorithm>
 
 namespace btq {
+    template<typename It>
+    void selectionSort(It begin, It end) {
+        for (auto i=begin; i<end; i++) {
+            std::iter_swap(i, std::min_element(i, end));
+        }
+    }
+
     template<typename T>
     void selectionSort(T *array, int a, int b) {
-        for (auto i=array+a; i<array+b; i++) {
-            std::iter_swap(i, std::min_element(i, array+b+1));
-        }
+        return btq::selectionSort(array+a, array+b+1);
     }
 
     template<typename T>
